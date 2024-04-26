43bbb74448adcae4efaf97886ca8e173b0692681
waasimAnsari786 <waasimansari92@gmail.com>
2024-04-25 18:17:13 -0700

    Update app.js
    I created that function which will print user age according to his date of birth. I also added some conditions, when they will be true then this function will execute.

--- app.js
+++ app.js
@@ -7,20 +7,64 @@ let date = new Date().getDate();
 let month = new Date().getMonth() + 1;
 let year = new Date().getFullYear();
 
+const printUserAge = (userAgeVal, userlifeDaysVal, systemYear, userYearVal) => {
+    userAgeVal.value = systemYear - userYearVal;
+    userlifeDaysVal.value = userAgeVal.value * 365;
+};
+
+const cnvtValInZero = (element) => {
+    element.value = `` ;
+};
+
 const splitUserDOBFunc = (element) => {
     let splitUserDOB = element.split(" ");
-    // userAge.value = year - splitUserDOB[2];
-    // lifeDays.value = userAge.value * 365;
-    
-    if (splitUserDOB[0] >= date && splitUserDOB[1] >= month && splitUserDOB[2] <= year){
-        console.log(`ok`);
+    let userDate = +splitUserDOB[0];
+    let userMonth = +splitUserDOB[1];
+    let userYear = +splitUserDOB[2];
+    let userYear2 = userYear + 1;
+
+    if (userDate <= 31 && userMonth <= 12 && userYear <= year) {
+
+        if (userDate > date && userMonth > month && userYear < year) {
+            printUserAge(userAge, lifeDays, year, userYear2);
+        }
+
+        else if (userDate < date && userMonth < month && userYear < year) {
+            printUserAge(userAge, lifeDays, year, userYear);
+        }
+
+        else if (userDate === date && userMonth === month && userYear < year) {
+            printUserAge(userAge, lifeDays, year, userYear);
+        }
+
+        else if (userDate <= date && userMonth > month && userYear < year) {
+            printUserAge(userAge, lifeDays, year, userYear2);
+        }
+
+        else if (userDate <= date && userMonth < month && userYear < year) {
+            printUserAge(userAge, lifeDays, year, userYear);
+        }
+
+        else if (userDate >= date && userMonth < month && userYear < year) {
+            printUserAge(userAge, lifeDays, year, userYear);
+        }
+
+        else if (userDate > date && userMonth === month && userYear < year) {
+            printUserAge(userAge, lifeDays, year, userYear2);
+        }
+
+        else if (userDate < date && userMonth === month && userYear < year) {
+            printUserAge(userAge, lifeDays, year, userYear);
+        }
+
     }
 
     else{
-        alert(`please enter a valid value`)
+        alert(`Please enter a valid Date Of Birth`);
+        cnvtValInZero(userDOB);
     }
 };
 
-submitBtn.addEventListener("click" , () => {
+submitBtn.addEventListener("click", () => {
     splitUserDOBFunc(userDOB.value);
 });
