74bf7ef558095094b4c0a1358f0515015de34494
waasimAnsari786 <waasimansari92@gmail.com>
2024-04-27 20:55:54 -0700

    Update app.js
    I updated the function of printing details of user's age and life days. Now this function will be print the details of user's age and life days with future date.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let userDOB = document.querySelector("#user-DOB");
 let lifeDays = document.querySelector("#life-days");
 let userAge = document.querySelector("#user-age");
 let submitBtn = document.querySelector("#submit-btn");
+let monthsNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 let date = new Date().getDate();
 let month = new Date().getMonth() + 1;
@@ -10,65 +11,87 @@ let year = new Date().getFullYear();
 let allInpAndPerasInner = document.querySelector(".all-inp-ctnr-inner");
 
 let createNewDiv = document.createElement("div");
-createNewDiv.classList.add("col-12" , "ctnr-of-inp-and-peras");
+createNewDiv.classList.add("col-12", "ctnr-of-inp-and-peras");
 allInpAndPerasInner.append(createNewDiv);
 
 let ageAndDaysInputSec = document.querySelector("#age-and-days-input-sec");
 createNewDiv.append(ageAndDaysInputSec);
 
 let createNewDiv2 = document.createElement("div");
-createNewDiv2.classList.add("col-12" , "ctnr-of-peras");
+createNewDiv2.classList.add("col-12", "ctnr-of-peras");
 createNewDiv.append(createNewDiv2);
 
 let newDiv = document.querySelector(".ctnr-of-inp-and-peras");
 let ctnrOfNewPeras = document.querySelector(".ctnr-of-peras");
 
-
 for (let index = 0; index < 2; index++) {
     let createnewPeras = document.createElement("p");
-    createnewPeras.classList.add("user-lifedays-and-age" , "mt-4");
-    createnewPeras.setAttribute("id" , `new-pera-${index + 1}`);
+    createnewPeras.classList.add("user-lifedays-and-age", "mt-4");
+    createnewPeras.setAttribute("id", `new-pera-${index + 1}`);
     createNewDiv2.append(createnewPeras);
 };
 
-const slider = (element , timer) => {
+const slider = (element, timer) => {
     setTimeout(() => {
         element.style.transform = `translateX(-100%)`;
     }, timer);
 };
 
-const printDetailsOfAgeAndLifeDays = (userAgeValue , userlifeDaysValue) => {
-    let str = `You are ${+userAgeValue} years old as well as if Allah wills you will be ${+userAgeValue + 1} years old!`;
-    
-    let str2 = `Your life days are ${+userlifeDaysValue} along with that if Allah wills you will complete your life's ${+userlifeDaysValue + 365} days!`;
-    
-    let newPera1 = createNewDiv2.children[0];
-    let newPera2 = createNewDiv2.children[1];
+const printMsg = (msgElement, msgElement2, userAgeValue , userlifeDaysValue , userDate , monthName , currYear) => {
+    let str = `You are ${+userAgeValue} years old as well as if Allah wills you will be ${+userAgeValue + 1} years old on ${userDate} ${monthName} ${currYear}!`;
 
-    newPera1.innerText = str;
-    newPera2.innerText = str2;
+    let str2 = `Your life days are ${+userlifeDaysValue} along with that if Allah wills you will complete your life's ${+userlifeDaysValue + 365} days on ${userDate} ${monthName} ${currYear}!`;
+    
+    msgElement.innerText = str;
+    msgElement2.innerText = str2;
 };
 
-const printUserAge = (userAgeVal , userlifeDaysVal , systemYear, userYearVal) => {
+const printDetailsOfAgeAndLifeDays = (userAgeVal, userlifeDaysVal, systemYear, userYearVal) => {
     userAgeVal.value = systemYear - userYearVal;
     userlifeDaysVal.value = userAgeVal.value * 365;
 
-    let finalAgeOfUser = userAgeVal.value;
-    let finalDaysOfUserLife = userlifeDaysVal.value;
-    
-    slider(ageAndDaysInputSec , 2000);
-    slider(ctnrOfNewPeras , 3000);
+    let userAgeValue = userAgeVal.value;
+    let userlifeDaysValue = userlifeDaysVal.value;
 
-    printDetailsOfAgeAndLifeDays(finalAgeOfUser , finalDaysOfUserLife);
+    let splitUserDOB = userDOB.value.split(" ");
+    let splitedUserDate = +splitUserDOB[0];
+    let splitedUserMonth = +splitUserDOB[1];
+    let splitedUserYear = +splitUserDOB[2];
+
+    let newPera1 = createNewDiv2.children[0];
+    let newPera2 = createNewDiv2.children[1];
+
+    monthsNames.map((monthsName, index) => {
+        let newIdx = index + 1;
+
+        if (splitedUserMonth === newIdx) {
+            if (splitedUserYear === userYearVal) {
+                printMsg(newPera1, newPera2, userAgeValue , userlifeDaysValue , splitedUserDate , monthsName , year + 1);
+            }
+
+            else if (splitedUserYear < userYearVal) {
+                printMsg(newPera1, newPera2, userAgeValue , userlifeDaysValue , splitedUserDate , monthsName , year);
+            }
+
+        }
+    });
 };
 
-const cnvtValInZero = (element , element2 , msg) => {
+const printUserAge = (userAgeVal, userlifeDaysVal, systemYear, userYearVal) => {
+    userAgeVal.value = systemYear - userYearVal;
+    userlifeDaysVal.value = userAgeVal.value * 365;
+
+    slider(ageAndDaysInputSec, 2000);
+    slider(ctnrOfNewPeras, 3000);
+};
+
+const cnvtValInZero = (element, element2, msg) => {
     alert(`${msg}`);
-    element.value = `` ;
-    element2.value = `` ;
+    element.value = ``;
+    element2.value = ``;
 };
 
-const printUserAge2 = (userDate , daysCountVal , userMonth , monthCountVal , systemYear) => {
+const printUserAge2 = (userDate, daysCountVal, userMonth, monthCountVal) => {
     for (let d = userDate; d < date; d++) {
         daysCountVal++;
     }
@@ -79,7 +102,7 @@ const printUserAge2 = (userDate , daysCountVal , userMonth , monthCountVal , sys
 
     userAge.value = ``;
     lifeDays.value = ``;
-    
+
     // alert(`You are ${daysCountVal} days old! \nYou are ${monthCountVal} months old! \nYour life days are ${monthCountVal * 30 + daysCountVal}!`);
 };
 
@@ -94,47 +117,54 @@ const splitUserDOBFunc = (element) => {
 
         if (userDate > date && userMonth > month && userYear < year) {
             printUserAge(userAge, lifeDays, year, userYear2);
-
+            printDetailsOfAgeAndLifeDays(userAge, lifeDays, year, userYear2);
         }
 
         else if (userDate < date && userMonth < month && userYear < year) {
             printUserAge(userAge, lifeDays, year, userYear);
+            printDetailsOfAgeAndLifeDays(userAge, lifeDays, year, userYear);
         }
 
         else if (userDate === date && userMonth === month && userYear < year) {
             printUserAge(userAge, lifeDays, year, userYear);
+            printDetailsOfAgeAndLifeDays(userAge, lifeDays, year, userYear);
         }
 
         else if (userDate <= date && userMonth > month && userYear < year) {
             printUserAge(userAge, lifeDays, year, userYear2);
+            printDetailsOfAgeAndLifeDays(userAge, lifeDays, year, userYear2);
         }
 
         else if (userDate <= date && userMonth < month && userYear < year) {
             printUserAge(userAge, lifeDays, year, userYear);
+            printDetailsOfAgeAndLifeDays(userAge, lifeDays, year, userYear);
         }
 
         else if (userDate >= date && userMonth < month && userYear < year) {
             printUserAge(userAge, lifeDays, year, userYear);
+            printDetailsOfAgeAndLifeDays(userAge, lifeDays, year, userYear);
         }
 
         else if (userDate > date && userMonth === month && userYear < year) {
             printUserAge(userAge, lifeDays, year, userYear2);
+            printDetailsOfAgeAndLifeDays(userAge, lifeDays, year, userYear2);
         }
 
         else if (userDate < date && userMonth === month && userYear < year) {
             printUserAge(userAge, lifeDays, year, userYear);
+            printDetailsOfAgeAndLifeDays(userAge, lifeDays, year, userYear);
         }
 
         else if (userDate > date && userMonth >= month && userYear === year) {
-            cnvtValInZero(userAge , lifeDays , `Correct your DOB's date or month because it's greater than today's date!`);
+            cnvtValInZero(userAge, lifeDays, `Correct your DOB's date or month because it's greater than today's date!`);
         }
 
         else if (userDate < date && userMonth > month && userYear === year) {
-            cnvtValInZero(userAge , lifeDays , `Correct your DOB's date or month because it's greater than today's date!`);
+            cnvtValInZero(userAge, lifeDays, `Correct your DOB's date or month because it's greater than today's date!`);
         }
 
         else if (userDate === date && userMonth === month && userYear === year) {
-            cnvtValInZero(userAge , lifeDays , `The DOB you provided to me, that's a new born baby's DOB who borned on today and he is just few hours old. You can ask me his age tomorrow or after few days or years. \n\nMay Allah bless him a good, healthy and long life with lots happiness and success!`);
+            cnvtValInZero(userAge, lifeDays, `The DOB you provided to me, that's a new born baby's DOB who borned on today and he is just few hours old. You can ask me his age tomorrow or after few days or years. \n\nMay Allah bless him a good, healthy and long life with lots happiness and success!`);
         }
 
         else if (userDate >= date && userMonth < month && userYear === year) {
@@ -142,27 +172,27 @@ const splitUserDOBFunc = (element) => {
             let monthCount = 1;
             let newDate = userDate - date;
 
-            printUserAge2(userDate , daysCount - newDate , userMonth + 1 , monthCount , year);
+            printUserAge2(userDate, daysCount - newDate, userMonth + 1, monthCount, year);
         }
 
         else if (userDate < date && userMonth < month && userYear === year) {
             let daysCount = 1;
             let monthCount = 1;
-                
-            printUserAge2(userDate , daysCount , userMonth + 1 , monthCount , year);
+
+            printUserAge2(userDate, daysCount, userMonth + 1, monthCount, year);
         }
 
         else if (userDate < date && userMonth === month && userYear === year) {
             let daysCount = 0;
             let monthCount = 0;
-                
-            printUserAge2(userDate , daysCount , userMonth + 1 , monthCount , year);
+
+            printUserAge2(userDate, daysCount, userMonth + 1, monthCount, year);
         }
 
     }
 
-    else{
-        cnvtValInZero(userAge , lifeDays , `Please enter a valid Date Of Birth`);
+    else {
+        cnvtValInZero(userAge, lifeDays, `Please enter a valid Date Of Birth`);
     }
 };
 
@@ -170,10 +200,10 @@ submitBtn.addEventListener("click", () => {
     splitUserDOBFunc(userDOB.value);
 });
 
-userAge.addEventListener("input" , () => {
-    cnvtValInZero(userAge , lifeDays , `You are unable to input any value in the input field!`);
+userAge.addEventListener("input", () => {
+    cnvtValInZero(userAge, lifeDays, `You are unable to input any value in the input field!`);
 });
 
-lifeDays.addEventListener("input" , () => {
-    cnvtValInZero(userAge , lifeDays , `You are unable to input any value in the input field!`);
+lifeDays.addEventListener("input", () => {
+    cnvtValInZero(userAge, lifeDays, `You are unable to input any value in the input field!`);
 });
